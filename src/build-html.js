1d58fa70295f09968c826546277e7c644b743517
Todey <chloe.e.hanks@gmail.com>
2022-12-20 02:41:57 -0600

    completed readme, altered build-html.js

--- src/build-html.js
+++ src/build-html.js
@@ -6,8 +6,8 @@
 const renderTeam = (team) => {
     // Create the manager card with the data inputs
     const renderManager = (manager) => `
-                <div class="card col-3 me-3 mb-5 employee-card">
-                    <div class="card-header employee-header">
+                <div class="card employee-card me-3 mb-5">
+                    <div class="card-header">
                         <h2 class="card-title">${manager.name}</h2>
                         <h3 class="card-title"><i class="fa-solid fa-mug-hot"></i> ${manager.getRole()}</h3>
                     </div>
@@ -26,8 +26,8 @@ const renderTeam = (team) => {
 
     // Create the engineer card with the data inputs
     const renderEngineer = (engineer) => `
-                <div class="card col-3 me-3 mb-5 employee-card">
-                    <div class="card-header employee-header">
+                 <div class="card employee-card me-3 mb-5">
+                     <div class="card-header">
                         <h2 class="card-title">${engineer.name}</h2>
                         <h3 class="card-title"><i class="fa-solid fa-glasses"></i> ${engineer.getRole()}</h3>
                     </div>
@@ -49,8 +49,8 @@ const renderTeam = (team) => {
 
     // Create the intern card with the data inputs
     const renderIntern = (intern) => `
-                <div class="card col-3 me-3 mb-5 employee-card">
-                    <div class="card-header employee-header">
+                <div class="card employee-card me-3 mb-5">
+                     <div class="card-header">
                         <h2 class="card-title">${intern.name}</h2>
                         <h3 class="card-title"><i class="fa-solid fa-user-graduate"></i> ${intern.getRole()}</h3>
                     </div>
@@ -108,33 +108,30 @@ const renderTeam = (team) => {
 const renderMain = (team) => `
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profiles</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./style.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Team Directory</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="team-profile-header col-12 mb-3 team-profile jumbotron">
-                <h1 class="team-profile-title text-center">Meet The Team</h1>
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">Team Directory</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-mem-container col-12 d-flex flex-wrap justify-content-around">
-                ${renderTeam(team)}
+            <div class="team-area col-12 d-flex justify-content-around">
+               ${renderTeam(team)}
             </div>
         </div>
     </div>
